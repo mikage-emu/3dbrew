6180cac0b0be6c9b7b22d83cf16b28eeab42a3cd
Tony Wasserka <neobrain@users.noreply.github.com>
2024-09-24 19:40:29 +0200

    Add script to strip unnecessary <p> for table headers

---
+++ 07_fix_table_headers.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+shopt -s globstar
+for i in content/**/*.md
+do
+  echo $i
+
+  # Table headers are wrapped in an unnecessary <p> tag
+  sed -i -e 's,<th><p>,<th>,g' $i
+  sed -i -e 's,</p></th>,</th>,g' $i
+done

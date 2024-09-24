eb5eb805ef254c953262f6ae54f888629d1a5cf0
Tony Wasserka <neobrain@users.noreply.github.com>
2024-09-24 15:55:45 +0200

    Add script to fix relative links and image sources

---
+++ 03_fix_image_refs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+shopt -s globstar
+CONTENTROOT=`realpath .`/content
+echo $CONTENTROOT
+for i in content/**/*.md
+do
+  echo $i
+
+  # Make HTML links and image sources relative to the root
+  PREFIX=`realpath --relative-to=$i $CONTENTROOT`
+
+  perl -0777 -i -pe 's,href="((?![/\.])(?!http).*)",href="'"$PREFIX"'/\1",g' $i
+
+  perl -0777 -i -pe 's,src="((?![/\.]).*)",src="'"$PREFIX"'/\1",g' $i
+done
